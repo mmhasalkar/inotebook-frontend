a2f0316980297c40635aa765be8fc200d05b3904
Mukunda Mhasalkar <mukunda.mhasalkar@ignitesol.com>
2021-09-09 20:09:00 +0530

    Creating separate components for Notes and NoteItem. Also updated the NoteState to contain sample notes data.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import Notes from "./Notes"
 
 const Home = () => {
     return (
@@ -22,9 +22,7 @@ const Home = () => {
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
-            <div className="container my-3">
-                <h1>Your notes</h1>
-            </div>
+            <Notes />
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,15 +2,86 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const s = {
-    }
 
-    // eslint-disable-next-line
-    const [state, setstate] = useState(s)
+    const notesInitial = [
+        {
+          "_id": "61375b475c6a2724a04020c2",
+          "user": "6134ee633a608887d6f3ff05",
+          "title": "Omkar's Second Note",
+          "description": "Second note description updated",
+          "tag": "Fudy",
+          "timestamp": "2021-09-07T12:29:59.893Z",
+          "__v": 0
+        },
+        {
+          "_id": "61375bb65c6a2724a04020c4",
+          "user": "6134ee633a608887d6f3ff05",
+          "title": "Second Note",
+          "description": "Second note description",
+          "tag": "Personal",
+          "timestamp": "2021-09-07T12:31:50.369Z",
+          "__v": 0
+        },
+        {
+            "_id": "61375b475c6a2724a04020c2",
+            "user": "6134ee633a608887d6f3ff05",
+            "title": "Omkar's Second Note",
+            "description": "Second note description updated",
+            "tag": "Fudy",
+            "timestamp": "2021-09-07T12:29:59.893Z",
+            "__v": 0
+          },
+          {
+            "_id": "61375bb65c6a2724a04020c4",
+            "user": "6134ee633a608887d6f3ff05",
+            "title": "Second Note",
+            "description": "Second note description",
+            "tag": "Personal",
+            "timestamp": "2021-09-07T12:31:50.369Z",
+            "__v": 0
+          },
+          {
+            "_id": "61375b475c6a2724a04020c2",
+            "user": "6134ee633a608887d6f3ff05",
+            "title": "Omkar's Second Note",
+            "description": "Second note description updated",
+            "tag": "Fudy",
+            "timestamp": "2021-09-07T12:29:59.893Z",
+            "__v": 0
+          },
+          {
+            "_id": "61375bb65c6a2724a04020c4",
+            "user": "6134ee633a608887d6f3ff05",
+            "title": "Second Note",
+            "description": "Second note description",
+            "tag": "Personal",
+            "timestamp": "2021-09-07T12:31:50.369Z",
+            "__v": 0
+          },
+          {
+            "_id": "61375b475c6a2724a04020c2",
+            "user": "6134ee633a608887d6f3ff05",
+            "title": "Omkar's Second Note",
+            "description": "Second note description updated",
+            "tag": "Fudy",
+            "timestamp": "2021-09-07T12:29:59.893Z",
+            "__v": 0
+          },
+          {
+            "_id": "61375bb65c6a2724a04020c4",
+            "user": "6134ee633a608887d6f3ff05",
+            "title": "Second Note",
+            "description": "Second note description",
+            "tag": "Personal",
+            "timestamp": "2021-09-07T12:31:50.369Z",
+            "__v": 0
+          },
+      ]
 
-
+      const [notes, setNotes] = useState(notesInitial)
+    
     return (
-        <NoteContext.Provider value={{state}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
